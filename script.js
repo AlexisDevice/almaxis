d7610c5dca2f19d9ab03d2b90dc3f79916ca7e01
Alexis <AlexisNoMain@icloud.com>
2025-05-05 16:55:09 -0400

    Algunos cambios

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getnum(maxNum, minNum) {
 }
 
 const answers = ["Si", "No", "Tal vez", "Puede ser", "Si y no",
-     "No deberia", "No lo se", "Tu ta loco/a", "Oye ahora", "Decansaaa"];
+     "No deberia", "No lo se"];
 
 function getAnswer() {
     label.textContent = answers[getnum(answers.length - 1, 0)];
